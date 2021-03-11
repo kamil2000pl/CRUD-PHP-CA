b6260393a34e9659cf48f9df257dedcc886c2060
kamil <kamiljozefowicz@gmail.com>
2021-03-11 14:16:38 +0000

    SASS setup

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <!-- the head section -->
 <head>
 <title>Keyboard Shop</title>
-<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 
 <!-- the body section -->
