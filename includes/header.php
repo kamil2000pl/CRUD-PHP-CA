4fca4c784814d75ca07249b7b096dd8344fbe068
kamil <kamiljozefowicz@gmail.com>
2021-04-23 00:36:33 +0100

    Added contact form html

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,7 @@
 <head>
 <title>Keyboard Shop</title>
 <link rel="stylesheet" type="text/css" href="css/main.css">
+<script language="JavaScript" src="scripts/gen_validatorv31.js" type="text/javascript"></script>
 </head>
 
 <!-- the body section -->
