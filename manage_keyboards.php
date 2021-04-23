730a985c4664b97e5bc26ed48686accc256b6542
kamil <kamiljozefowicz@gmail.com>
2021-04-23 02:14:02 +0100

    Fixed changing category a admin returning to index

--- manage_keyboards.php
+++ manage_keyboards.php
@@ -74,7 +74,7 @@ $statement3->closeCursor();
         <nav>
             <ul>
                 <?php foreach ($categories as $category) : ?>
-                    <li><a href=".?category_id=<?php echo $category['categoryID']; ?>">
+                    <li><a href="?category_id=<?php echo $category['categoryID']; ?>">
                             <?php echo $category['categoryName']; ?>
                         </a>
                     </li>
